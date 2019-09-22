103ab94e4a9875eb7a376b1e27e3985ba4e167c2
Rpaca <paysage11@naver.com>
2019-09-22 11:55:29 +0900

    Assignment01
    Unity의 Tag기능을 이용하여 가장 가까운적을 찾는 게임을 제작하였습니다.

---
+++ ZombiWorld/Assets/Script/DistanceCalcurator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceCalcurator : MonoBehaviour
+{
+    public List<GameObject> foundObjects;
+    public GameObject enemy;
+    public string tagName;
+    public float shortDis;
+
+
+    void Update()
+    {
+        foundObjects = new List<GameObject>(GameObject.FindGameObjectsWithTag(tagName));
+        shortDis = Vector3.Distance(gameObject.transform.position, foundObjects[0].transform.position); // 첫번째를 기준으로 잡아주기 
+
+        enemy = foundObjects[0]; // 첫번째를 먼저 
+
+        foreach (GameObject found in foundObjects)
+        {
+            float Distance = Vector3.Distance(gameObject.transform.position, found.transform.position);
+
+            if (Distance < shortDis) // 위에서 잡은 기준으로 거리 재기
+            {
+                enemy = found;
+                shortDis = Distance;
+            }
+        }
+        Debug.Log(enemy.name);
+        Debug.Log(shortDis);
+    }
+}
+
