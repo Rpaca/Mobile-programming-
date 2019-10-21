90c9b12cf5142d537ef8aa33eeb95e6ee5dee898
Rpaca <paysage11@naver.com>
2019-10-21 23:09:41 +0900

    Trtris Game -1-
    기본 블록 생성

---
+++ Tetris/Assets/Scripts/Blocks.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blocks : MonoBehaviour {
+
+    private float speed = 1.0f;
+    private float lastTime = 0;
+    private float speedTime = 0.8f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        moveDown();
+
+	}
+
+    private void moveDown()
+    {
+        //바닥에 떨어질때가지 계속 떨어짐
+        if (transform.position.y == 0)
+            return;
+        if (Time.time - lastTime >= speedTime)
+        {
+            transform.position += Vector3.down;
+            lastTime = Time.time;
+        }
+    }
+}
