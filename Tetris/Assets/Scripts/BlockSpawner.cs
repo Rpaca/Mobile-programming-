7e511efd1f04fe74f8431dcbaac9afaf693e3460
Rpaca <paysage11@naver.com>
2019-10-26 23:16:55 +0900

    Tetris BlockSpawner 추가
    1. 7종류의 block prefabs 생성
    2. blocks 코드에 블록 이동 관련 기능 추가
    3. blockSpanwer 추가(생성 시간 관련 수정 필요)

---
+++ Tetris/Assets/Scripts/BlockSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour
+{
+
+    public GameObject[] blockPrefabs;
+    private GameManager GM = GameObject.FindGameObjectWithTag("gameManager").GetComponent<GameManager>();
+
+    // Use this for initialization
+    void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       // if (GM.isSpawning == false)
+            spawn();		
+	}
+
+    private void spawn()
+    {
+        GameObject tempt;
+        Vector3 pos = this.gameObject.transform.position;
+        int i = Random.Range(0, blockPrefabs.Length);
+        tempt = Instantiate(blockPrefabs[i], pos, Quaternion.identity);
+        GM.isSpawning = true;
+    }
+}
--- Tetris/Assets/Scripts/Blocks.cs
+++ Tetris/Assets/Scripts/Blocks.cs
@@ -2,33 +2,103 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Blocks : MonoBehaviour {
-
-    private float speed = 1.0f;
+public class Blocks : MonoBehaviour
+{
     private float lastTime = 0;
-    private float speedTime = 0.8f;
+    private float timeInterval = 1.5f;
+    private bool isActive = true;
+    private GameManager GM = GameObject.Find("GameManager").GetComponent<GameManager>();
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
 
-        moveDown();
-
-	}
-
-    private void moveDown()
+    // Use this for initialization
+    void Start()
     {
-        //바닥에 떨어질때가지 계속 떨어짐
-        if (transform.position.y == 0)
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        drop();
+
+        if (isActive == false)
             return;
-        if (Time.time - lastTime >= speedTime)
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            horizontalMove(true);
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            horizontalMove(false);
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            rotationalMove();
+        if (Input.GetKey(KeyCode.DownArrow))
+            speedUp(true);
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+            speedUp(false);
+        if (Input.GetKeyDown(KeyCode.Space))
+            instantDrop();
+
+  
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "blocks")
+        {
+            print("crush");
+            GetComponent<Blocks>().enabled = false;
+        }
+    }
+
+
+    //자유낙하
+    private void drop()
+    {
+        //바닥에 도착했는지 확인
+        if (transform.position.y == 0)
+        {
+            GM.isSpawning = false;
+            return;
+        }
+
+        if (Time.time - lastTime >= timeInterval)
         {
             transform.position += Vector3.down;
             lastTime = Time.time;
         }
+    }
+
+    //좌우 이동
+    private void horizontalMove(bool direction)
+    {
+        //충돌했는지 확인
+
+        //아닐시
+        if(direction == true)
+            transform.position += Vector3.left;
+        else
+            transform.position += Vector3.right;
+    }       
+
+
+    //회전
+    private void rotationalMove()
+    {
+        transform.Rotate(0, 0, -90);
+    }
+
+    //스피드 업
+    private void speedUp(bool onActive)
+    {
+        timeInterval = 1.5f;
+        if (onActive == true)
+            timeInterval = 0.1f;
+    }
+
+    //즉시낙하
+    private void instantDrop()
+    {
+        timeInterval = 0;
+        isActive = false;
     }
 }
